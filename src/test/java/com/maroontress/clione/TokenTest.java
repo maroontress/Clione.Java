eff5c01112b0356702c38f5d82c808d41b25e1e1
Tomohisa Tanaka <Tomohisa.Tanaka@gmail.com>
2025-08-27 16:53:26 +0900

    Refactor

--- src/test/java/com/maroontress/clione/TokenTest.java
+++ src/test/java/com/maroontress/clione/TokenTest.java
@@ -1,11 +1,11 @@
 package com.maroontress.clione;
 
-import com.maroontress.clione.impl.DefaultToken;
+import com.maroontress.clione.impl.SourceChars;
+import com.maroontress.clione.impl.TokenBuilder;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -13,10 +13,13 @@ import static org.hamcrest.Matchers.is;
 public final class TokenTest {
 
     private Token createToken(String value, TokenType type) {
-        var chars = value.chars()
-                .mapToObj(c -> new TestSourceChar((char) c))
-                .collect(Collectors.<SourceChar>toList());
-        return new DefaultToken(chars, type);
+        var builder = new TokenBuilder();
+        var size = value.length();
+        for (var k = 0; k < size; ++k) {
+            var c = SourceChars.of(value.charAt(k), 1 + k, 1);
+            builder.append(c);
+        }
+        return builder.toToken(type);
     }
 
     @Test
@@ -40,30 +43,5 @@ public final class TokenTest {
         var token = createToken("if", TokenType.RESERVED);
         assertThat(token.isValue("if"), is(true));
         assertThat(token.isValue("else"), is(false));
-    }
-
-    // A simple implementation of SourceChar for testing.
-    private record TestSourceChar(char c) implements SourceChar {
-
-        @Override
-        public char toChar() {
-            return c;
-        }
-
-        @Override
-        public SourceSpan getSpan() {
-            // Not needed for these tests.
-            return null;
-        }
-
-        @Override
-        public List<SourceChar> getChildren() {
-            return List.of();
-        }
-
-        @Override
-        public boolean isEof() {
-            return false;
-        }
     }
 }
